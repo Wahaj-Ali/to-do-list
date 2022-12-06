e23f150b80f9c6dd40fc82ac608aae8d082087ac
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-12-06 21:12:42 +0500

    updated package.json

--- dist/runtime.bundle.js
+++ dist/runtime.bundle.js
@@ -182,7 +182,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkwebpack_setup"] = self["webpackChunkwebpack_setup"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();

2ff75bbbf2fc472e0a36e1dae727a640bffdf65e
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-12-07 20:17:27 +0500

    in this branch implemented add, remove and modify tasks functionalities.
    in this commit
    - added a function to add a new task
    - added a function to remove tasks
    - added a function to edit current tasks
    - stored tasks in local storage
    - after deletion set the proper indexes for remaining tasks.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,158 @@
 /* eslint-disable */
-import _ from 'lodash'; 
+import _, { indexOf } from 'lodash'; 
 
 /* eslint-enable */
 import './style.css';
 
-const simpleTasks = [
-  {
-    description: 'Complete the structure of To-do-list',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Complete setting up webpack',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Complete the all pending tasks. Also complete other pending tasks. Make sure ton complete on time. Also define what you did',
-    completed: false,
-    index: 3,
-  },
-];
+class listTask {
+  allTasks = [];
 
-function displayTasks() {
-  let result = '';
-  simpleTasks.filter((task) => task.index > 0).forEach((task) => {
-    result += `
-    <div class="item">
-    <div class="itme-desc">
-    <input type="checkbox"  name="${task}" >
-    <label for="task"> ${task.description}</label><br>
-    </div>
-    <div class="item-btn">
-      <i class="fa-solid fa-xmark"></i>
-    </div>
-  </div> `;
-  });
-  document.querySelector('.tasks').innerHTML = result;
+  addTask(description) {
+    const task = {
+      description,
+      completed: false,
+      index: this.allTasks.length + 1,
+    };
+    this.allTasks.push(task);
+    this.saveTask();
+  }
+
+  deleteTask(index) {
+    this.allTasks.splice(index-1 , 1);
+    this.updateIndex();
+    this.saveTask();
+  }
+
+  updateIndex() {
+    this.allTasks.map((a) => {
+      a.index = this.allTasks.indexOf(a) + 1;
+      return a;
+    });
+  }
+
+  saveTask() {
+    const tasks = JSON.stringify(this.allTasks);
+    localStorage.setItem('tasks', tasks);
+  }
+
+  getStoredTodos() {
+    this.allTasks = JSON.parse(
+      localStorage.getItem('tasks'),
+    );
+  }
+
+  completedTask(status, index) {
+    this.allTasks[index - 1].completed = status;
+    this.saveTask();
+  }
+
+  editTask(paragraph, index) {
+    this.allTasks[index - 1].description = paragraph.textContent;
+    this.saveTask();
+  }
+
+  getStoredTasks() {
+    this.allTasks = JSON.parse(
+      localStorage.getItem('tasks'),
+    );
+  }
 }
 
-displayTasks();
+const todo = new listTask();
+const listSection = document.querySelector('.tasks');
+const form = document.getElementById('form');
+
+//show tasks
+const displayTasks = () => {
+  listSection.replaceChildren();
+  if (todo.allTasks.length > 0) {
+    listSection.style.display = 'block';
+    const listContainer = document.createElement('ul');
+    listContainer.className = 'item';
+    listSection.appendChild(listContainer);
+    todo.allTasks.map((a) => {
+      const list = document.createElement('li');
+      list.className = 'itme-desc';
+
+      const descrptContainer = document.createElement('div');
+      descrptContainer.className = 'task-div';
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.id = 'checkbox';
+      if (a.completed === true) { checkbox.checked = 'checked'; }
+
+      checkbox.onclick = (e) => {
+        todo.completedTask(e.target.checked, a.index);
+      };
+      descrptContainer.appendChild(checkbox);
+
+      const descrpt = document.createElement('p');
+      descrpt.id = 'taskLabel';
+      descrpt.textContent = a.description;
+      descrptContainer.appendChild(descrpt);
+      list.appendChild(descrptContainer);
+
+      const deleteIcon = document.createElement('i');
+      deleteIcon.className = 'fa-solid fa-xmark';
+      deleteIcon.id = a.index;
+      list.appendChild(deleteIcon);
+
+      list.onclick = () => {
+        descrpt.contentEditable = 'true';
+      };
+      listContainer.append(list);
+      return list;
+    });
+    document.createElement('p').addEventListener('keyup', (e) => {
+      if (e.target.id === 'task-description') {
+        if (e.key === 'Enter') {
+          displayTasks();
+        } else {
+          todo.editTask(e.target, a.index);
+        }
+      }
+    });
+    listSection.appendChild(listContainer);
+  }
+};
+
+//add a task
+const getAddedTodos = () => {
+  const newTodo = document.getElementById('task-info');
+  const description = newTodo.value;
+  if (description !== '') {
+    todo.addTask(description);
+    displayTasks();
+    newTodo.value = '';
+  }
+};
+form.addEventListener('submit', getAddedTodos);
+
+//delete a task
+ const delTask = (index) => {
+  todo.deleteTask(index);
+  todo.saveTask();
+  displayTasks();
+};
+
+document.addEventListener('click', (e) => {
+  if (e.target.className === 'fa-solid fa-xmark') {
+    delTask(e.target.id);
+  }
+});
+
+//get tasks from localstorage
+
+const populate = () => {
+  if (localStorage.getItem('tasks')) {
+    todo.getStoredTasks();
+    displayTasks();
+  } else {
+    displayTasks();
+  }
+};
+
+populate();
+
+
+

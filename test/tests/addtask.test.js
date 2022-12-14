a83a42e4013c8f0b87b4416a6b243042dd51fa98
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-12-14 22:14:14 +0500

    Update addtask.test.js
    removed linter errors.

--- test/tests/addtask.test.js
+++ test/tests/addtask.test.js
@@ -5,61 +5,58 @@ import delTask from "../../src/modules/deltask";
 
 
 function addTaskDescription(val, input) {
-    input.value = val;
+  input.value = val;
 }
 
 describe('This test', () => {
-    test('creates a container element inside the listSection', () => {
-        createMockDocument('description-1');
-        localStorage.clear();
-        const listSection = document.querySelector('.tasks');
-    
-        getAddedTodos();
-    
-        expect(listSection.children[0].className).toEqual(
-          'item',
-        );
-      });
+  test('creates a container element inside the listSection', () => {
+    createMockDocument('description-1');
+    localStorage.clear();
+    const listSection = document.querySelector('.tasks');
 
-      test('creates a task list element inside the list container', () => {
-        todo.allTodos = [];
-        createMockDocument('description-2');
-        // localStorage.clear();
-        const listSection = document.querySelector('.tasks');
-    
-        getAddedTodos();
-    
-        expect(
-          listSection.children[0].children[0].className,
-        ).toEqual('itme-desc');
-      });
+    getAddedTodos();
 
-    
-      test('should remove 1st task in the list container', () => {
-        todo.allTasks = [];
-        createMockDocument('task1');
+    expect(listSection.children[0].className).toEqual(
+      'item',
+    );
+  });
 
-        const listSection = document.querySelector('.tasks');
-        getAddedTodos();
-        delTask(1);
-        expect(listSection.children.length).toEqual(0);
-      });
-      
-      test('adds the exact number of task elements to the list container ', () => {
-        todo.allTasks = [];
-        createMockDocument('task1');
-        // global.localStorage.clear();
-    
-        const listSection = document.querySelector('.tasks');
-        const newTodo = document.getElementById('task-info');
-    
-        getAddedTodos();
-        addTaskDescription('description-4', newTodo);
-        getAddedTodos();
-        addTaskDescription('description-5', newTodo);
-        getAddedTodos();
-        expect(listSection.children[0].children.length).toEqual(
-          3,
-        );
-      });
+  test('creates a task list element inside the list container', () => {
+    todo.allTodos = [];
+    createMockDocument('description-2');
+    const listSection = document.querySelector('.tasks');
+
+    getAddedTodos();
+
+    expect(
+      listSection.children[0].children[0].className,
+    ).toEqual('itme-desc');
+  });
+
+  test('should remove 1st task in the list container', () => {
+    todo.allTasks = [];
+    createMockDocument('task1');
+
+    const listSection = document.querySelector('.tasks');
+    getAddedTodos();
+    delTask(1);
+    expect(listSection.children.length).toEqual(0);
+  });
+
+  test('adds the exact number of task elements to the list container ', () => {
+    todo.allTasks = [];
+    createMockDocument('task1');
+  
+    const listSection = document.querySelector('.tasks');
+    const newTodo = document.getElementById('task-info');
+
+    getAddedTodos();
+    addTaskDescription('description-4', newTodo);
+    getAddedTodos();
+    addTaskDescription('description-5', newTodo);
+    getAddedTodos();
+    expect(listSection.children[0].children.length).toEqual(
+      3,
+    );
+  });
 });
